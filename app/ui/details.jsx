ef94d44c7c51833fd79b189859c69accff5490d1
Kofiastro <astrokofi@gmail.com>
2024-07-15 11:38:19 +0000

    rounded corners fixed

--- app/ui/details.jsx
+++ app/ui/details.jsx
@@ -3,7 +3,7 @@ export default function Details() {
   return (
     <div className='  relative mx-[24px] md:mx-[40px] lg:mx-auto max-w-3xl flex justify-center items-center '>
       {/* job card details */}
-      <div className='relative bg-White w-full mx-auto flex rounded-lg flex-col md:flex-row items-center md:justify-between  md:space-y-0 '>
+      <div className='relative bg-White w-full mx-auto flex rounded-lg  md:rounded-none md:rounded-br-lg flex-col md:flex-row items-center md:justify-between  md:space-y-0 '>
         <Image
           src={'/logos/scoot.svg'}
           width={140}
@@ -18,7 +18,7 @@ export default function Details() {
           className='bg-yellow-600 px-[5px] py-[19px]  relative -top-5 rounded-xl block md:hidden '
         />
         <div className='flex flex-col items-center justify-center space-y-[13px] md:space-y-0 my-[24px] md:my-0 '>
-          <h1 className='font-bold text-lg text-VeryDarkBlue '>Scoot</h1>
+          <h1 className='font-bold text-xl text-VeryDarkBlue '>Scoot</h1>
           <h1 className='font-light text-lg text-Grey '>scoot.com</h1>
         </div>
         <div>
